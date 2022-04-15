cd152383e2da36161c43820aca34a3602893f0dd
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-15 09:20:47 -0700

    sdlc problems 1 and 2 done

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +18,14 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let planning = `This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.`;
+
+let analysisOfRequirements = `Requirements analysis or requirements engineering is a process used to determine the needs and expectations of a new product. It involves frequent communication with the stakeholders and end-users of the product to define expectations, resolve conflicts, and document all the key requirements.`;
+
+let design = `The design phase of software development deals with transforming the customer requirements as described in the SRS documents into a form implementable using a programming language.`;
+
+let implementation = `The software implementation stage involves the transformation of the software technical data package (TDP) into one or more fabricated, integrated, and tested software configuration items that are ready for software acceptance testing.`;
+
+let testingIntegration = `Testing is an integral component of the software development process. It entails a comprehensive assessment of a software to ensure it meets your client's requirements and goals. The primary goal of testing is to identify all the defects and errors in the software.`;
+
+let maintenance = `Software maintenance is the process of changing, modifying, and updating software to keep up with customer needs. Software maintenance is done after the product has launched for several reasons including improving the software overall, correcting issues or bugs, to boost performance, and more.`;

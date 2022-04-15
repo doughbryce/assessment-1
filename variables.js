a2bf9ff261003b0cb8ba15f7ffd21e4655ff2313
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-04-15 09:24:58 -0700

    variables problems 1-3 done

--- variables.js
+++ variables.js
@@ -1,17 +1,21 @@
+// const log = (arg) => console.log(arg)
 //////////////////PROBLEM 1////////////////////
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
-//CODE HERE 
+let myName = `bryce doughman`;
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let faveNum = 3;
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
-//CODE HERE
+let lovesCode = true;
+let areYouSureAboutThat = `definitely`;
+
+// console.log(myName, faveNum, lovesCode, areYouSureAboutThat);
 
 //////////////////PROBLEM 4////////////////////
 /*
